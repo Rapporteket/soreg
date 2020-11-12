073cb5218dd969b10e421a94663f9db8d9ffea19
Hannu Lyyjynen <hapu66@gmail.com>
2020-11-12 08:49:48 +0100

    page structure?

--- inst/gamle.R
+++ inst/gamle.R
@@ -9,12 +9,12 @@ library(purrr)
 library(stringr)
 library(magrittr)
 
-library(rapwhale)
+# library(rapwhale)
 library(flexdashboard)
 library(shinyWidgets)
 
 
-d_full <- arsrp("soreg")
+d_full <- soreg::get_arsrp("soreg")
 
 # Legg til info om operasjonsår
 d_full %<>% mutate(op_aar = year(Operasjonsdato),
@@ -47,29 +47,29 @@ d_full = d_full %>% mutate(bmi_baseline = BR_Vekt/(BR_Hoyde/100)^2,
 # frå 00:00 til 24:00, så me ville mista det siste døgnet).)
 rapporteringsaar = 2019
 dato_opptilrapaar = as_date(paste0(rapporteringsaar, "-12-31")) # For ev. seinare bruk i teksten
-d_opptilrapaar = d_full %>% 
+d_opptilrapaar = d_full %>%
   filter(Operasjonsdato < (dato_opptilrapaar + 1))
 
 # Data for alle år opptil (og inkludert) to år før rapporteringsåret,
 # dvs. alle data me i teorien burde ha toårs oppfølgingsdata på
 dato_toaarsdata = as_date(paste0(rapporteringsaar - 2, "-12-31")) # For ev. seinare bruk i teksten
-d_toaarsdata = d_full %>% 
+d_toaarsdata = d_full %>%
   filter(Operasjonsdato < (dato_toaarsdata + 1))
 
 # Og tilsvarande for eittårsdata
 dato_eittaarsdata = as_date(paste0(rapporteringsaar - 1, "-12-31")) # For ev. seinare bruk i teksten
-d_eittaarsdata = d_full %>% 
+d_eittaarsdata = d_full %>%
   filter(Operasjonsdato < (dato_eittaarsdata + 1))
 
 # Dei som vart operert det aktuelle året
-d = d_full %>%  filter(op_aar == rapporteringsaar)
+d = d_full #  %>%  filter(op_aar == rapporteringsaar)
 
 # Talet på operasjonsmåtar
 n_op = nrow(d_full)
 n_pas = n_distinct(d$PasientID)
 
-# d_prim = d %>% filter(op_primar)  
-d_prim = d_full %>% filter(op_primar)    
+# d_prim = d %>% filter(op_primar)
+d_prim = d_full %>% filter(op_primar)
 
 
 # I nokre analysar ser me berre på dei som har 6-vekesoppfølging
@@ -83,7 +83,7 @@ maksdogn_vis = 14
 # Talet på primæroperasjonar og reoperasjonar
 n_prim = nrow(d_prim)
 n_reop = n_op - n_prim
- 
+
 
 # Kva var den vanlegaste talet døgn å ligga etter operasjon
 liggedogn_typetal = d_prim_6v %>%
@@ -95,29 +95,72 @@ liggedogn_typetal = d_prim_6v %>%
 # funksjon for å regne ut kvalitetsindikatoren
 # definert for liggetid (andel pasienter med 3 dager eller færre)
 ki_liggetid = function(df){
-  
+
   teljar = sum(df$LiggeDogn <= 3)
   nemnar = nrow(df)
-  
+
   ind = teljar/nemnar
-  
+
   res = tibble(# forklaring="Del pasienter med 3 eller færre liggedøgn",
     # ltx_tekst = "Tre/færre liggedøgn",
     teljar, nemnar, ind = ind) # trenger ikke å definere at
   # variabler som er med i en group_by
   # skal være med, de kommer uansett
-  res            
+  res
 }
 
 # Kor mange låg mindre enn fire døgn
 p_kortligg = ki_liggetid(d_prim_6v)$ind
 
 # Kor mange låg mindre enn fire døgn per sjukehus
-d_kortligg_sjuk = d_prim_6v %>% 
-  group_by(OperererendeSykehus, op_aar) %>% 
-  do(ki_liggetid(.)) %>% 
+d_kortligg_sjuk = d_prim_6v %>%
+  group_by(OperererendeSykehus, op_aar) %>%
+  do(ki_liggetid(.)) %>%
   arrange(desc(ind))
 
 kortligg_sh <- function(sh) {d_kortligg_sjuk %>% filter(OperererendeSykehus %in% sh)}
 kortligg_yr <- function(yr) {d_kortligg_sjuk %>% filter(op_aar %in% yr)}
 kortligg    <- function(sh,yr){d_kortligg_sjuk %>% filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+
+# shiny inputs
+min_dato <-min(d_full$Operasjonsdato)
+max_dato <-max(d_full$Operasjonsdato)
+fyrstAar<-year(min_dato)
+sistAar<-year(max_dato)
+
+pickerInput(
+  inputId = "sh",
+  label = "velg sjukehus",
+  choices =  c("Helse Bergen","Helse Stavanger",
+               "Testsjukhus Norge"),   # unique(d_full$OperererendeSykehus),  #
+  selected = "Testsjukhus Norge",
+  multiple = TRUE,
+  options = pickerOptions(
+    actionsBox = TRUE,
+    title = "Please select a hospital",
+    header = "This is a list of hospitals"
+  ))
+# checkboxGroupInput(inputId = "lggar", label ="år", choices=c(2017,2018)),  # multiple?
+# selectInput(inputId= "vrb", label = "Variabel:", c("bmi_baseline")),
+# sliderInput("bn",  label = "Antall grupper:",
+#             min = 1,
+#             max = 10,
+#             value = 5)
+# ),
+# mainPanel(          tabsetPanel(
+#   tabPanel("Plot en variabel",      plotOutput("PlotKI1")),
+#   tabPanel("Data table", tableOutput("TableKI1")),
+#   tabPanel("own table", dataTableOutput('ligge'))
+# )
+# ))
+# ),
+# # sliderInput("ixs", label ="år", min=fyrstAar, max=sistAar, value=c(2017,2018), step=1)
+# #   checkboxGroupInput(inputId = "aar", label ="år", choices=c(2017,2018)) , # multiple?
+# # radioButtons("år",label ="years", fyrstAar:sistAar)  # multiple?
+# # selectInput("sykeh", label="sykehus", unique(dt$OperererendeSykehus),multiple=TRUE)
+# # selectInput("n_breaks", label = "Number of bins:",
+# #            choices = c(10, 20, 35, 50), selected = 20)
+# dateRangeInput('dateRange',
+#                label = 'Datointerval: yyyy-mm-dd',
+#                start = min_dato, end = max_dato)
+#
