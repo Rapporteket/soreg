70b2cda2a23915cee860d821f33cbebe26878206
Are Edvardsen <biorakel@gmail.com>
2020-11-13 14:46:49 +0100

    add pipe operator in utils

--- R/utils.R
+++ R/utils.R
@@ -22,6 +22,18 @@ NULL
 #' @usage lhs \%<>\% rhs
 NULL
 
+#' Exposition pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%$\%}} for details.
+#'
+#' @name %$%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %$%
+#' @usage lhs \%$\% rhs
+NULL
+
 #' Data pronouns
 #'
 #' See \code{rlang::\link[rlang:tidyeval-data]{.data}} for details
