20d8c9b77d81d18e6c379bf066a441b7fba55ef8
Hannu Lyyjynen <hapu66@gmail.com>
2020-11-19 14:07:09 +0100

    Lines should not be more than 80 characters.

--- inst/shinyApps/soreg/server.R
+++ inst/shinyApps/soreg/server.R
@@ -10,20 +10,21 @@
 # library(rapbase)
 
 server <- function(input, output, session) {
-
+#----------------------------------------------------------------------------80
   # Faste verdier for applikasjonen
   registry_name <- "soreg"
   # Last inn data
   # Legg til info om operasjonsår og primæroperasjon
    d_full <- soreg::get_arsrp("soreg")
-    d_full %<>% dplyr::mutate(op_aar = lubridate::year(Operasjonsdato), op_primar = (TidlFedmeOp==0))
-    # d_prim = d %>% filter(op_primar)
+    d_full %<>% dplyr::mutate(
+      op_aar = lubridate::year(Operasjonsdato),
+      op_primar = (TidlFedmeOp==0))
    d_prim <- d_full %>% dplyr::filter(op_primar)
   # I nokre analysar ser me berre på dei som har 6-vekesoppfølging
   # registrert. Hentar ut eiga datasett for desse
    d_prim_6v <- d_prim %>% dplyr::filter(`6U_KontrollType` %in% 1:3)
 
-  # tidsinterval
+# tidsinterval
    min_dato <-min(d_full$Operasjonsdato)    ## hvordan kan disse sendes til ui?
    max_dato <-max(d_full$Operasjonsdato)
    fyrstAar<-lubridate::year(min_dato)
@@ -36,10 +37,15 @@ server <- function(input, output, session) {
     dplyr::group_by(OperererendeSykehus, op_aar) %>%
     dplyr::do(soreg::ki_liggetid(.)) %>%
     dplyr::arrange(desc(ind))
-
-  kortligg_sh <- function(sh) {d_kortligg_sjuk %>% dplyr::filter(OperererendeSykehus %in% sh)}
-  kortligg_yr <- function(yr) {d_kortligg_sjuk %>% dplyr::filter(op_aar %in% yr)}
-  kortligg    <- function(sh, yr){d_kortligg_sjuk %>% dplyr::filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+#----------------------------------------------------------------------------80
+kortligg_sh <- function(sh){
+  d_kortligg_sjuk %>% dplyr::filter(OperererendeSykehus %in% sh)}
+kortligg_yr <- function(yr){
+  d_kortligg_sjuk %>% dplyr::filter(op_aar %in% yr)}
+kortligg    <- function(sh, yr){
+  d_kortligg_sjuk %>% dplyr::filter(
+    OperererendeSykehus %in% sh,
+    op_aar %in% yr)}
 
 # REINNLEGGELSE
 # I analysar for reinnlegging ser me berre på dei som har 6-vekesoppfølging
@@ -48,72 +54,82 @@ server <- function(input, output, session) {
 # indikatoren i årsrapporten). Hentar ut eiga datasett for desse. Men det
 # viser seg at det òg er mogleg å svara «Vet ikke» (verdi 2) på om pasienten
 # vart reinnlagd. Desse gjev ingen informasjon, og vert derfor òg fjerna.
-d_reinn = d_prim %>% dplyr::filter(((`6U_KontrollType` %in% 1:3) |
+d_reinn <- d_prim %>% dplyr::filter(((`6U_KontrollType` %in% 1:3) |
                              (`6U_Behandling30Dager` == 1)) &
                              (`6U_Behandling30Dager` != 2))
 
 # Tilsvarande for alvorlege komplikasjonar
-d_kompl = d_prim %>% dplyr::filter((`6U_KontrollType` %in% 1:3) | (!is.na(`6U_KomplAlvorGrad`)))
+d_kompl <- d_prim %>%
+  dplyr::filter((`6U_KontrollType` %in% 1:3) | (!is.na(`6U_KomplAlvorGrad`)))
 
-
-
-
-# andel pasienter som får innleggelse innen 30 dager etter operasjon per sjukehus
-d_innlegg30 = d_reinn %>%
-              dplyr::group_by(OperererendeSykehus, op_aar) %>%
-			  dplyr::do(soreg::ki_30dager(.)) %>%
-			  dplyr::arrange(desc(ind))
-innl30_sh <- function(sh) {d_innlegg30 %>% filter(OperererendeSykehus %in% sh)}
-innl30_yr <- function(yr) {d_innlegg30 %>% filter(op_aar %in% yr)}
-innl30 <- function(sh,yr){d_innlegg30 %>% filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+#----------------------------------------------------------------------------80
+# andel pasienter som får innleggelse innen 30 dager etter operasjon
+# per sjukehus
+d_innlegg30 <- d_reinn %>%
+  dplyr::group_by(OperererendeSykehus, op_aar) %>%
+	dplyr::do(soreg::ki_30dager(.)) %>%
+	dplyr::arrange(desc(ind))
+innl30_sh <- function(sh){d_innlegg30 %>%
+    filter(OperererendeSykehus %in% sh)}
+innl30_yr <- function(yr){d_innlegg30 %>%
+    filter(op_aar %in% yr)}
+innl30 <- function(sh,yr){d_innlegg30 %>%
+    filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
 
 # KOMPLIKASJONAR
 
 # andel pasienter som får en alvorlig komplikasjon per sjukehus
-d_kompl_alv_sjukehus = d_kompl %>%
-                       dplyr::group_by(OperererendeSykehus,op_aar) %>%
-					             dplyr::do(soreg::ki_kompl_alv(.)) %>%
-					             dplyr::arrange(desc(ind))
+d_kompl_alv_sjukehus <- d_kompl %>%
+  dplyr::group_by(OperererendeSykehus,op_aar) %>%
+	dplyr::do(soreg::ki_kompl_alv(.)) %>%
+	dplyr::arrange(desc(ind))
 
-kompl_sh <- function(sh) {d_kompl_alv_sjukehus %>% filter(OperererendeSykehus %in% sh)}
-kompl_yr <- function(yr) {d_kompl_alv_sjukehus %>% filter(op_aar %in% yr)}
-kompl <- function(sh, yr){d_kompl_alv_sjukehus %>% filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+kompl_sh <- function(sh){d_kompl_alv_sjukehus %>%
+    filter(OperererendeSykehus %in% sh)}
+kompl_yr <- function(yr){d_kompl_alv_sjukehus %>%
+    filter(op_aar %in% yr)}
+kompl <- function(sh, yr){d_kompl_alv_sjukehus %>%
+    filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
 
-  # aarskontrollar
-  ####################################################
-  # +-90                                             #  slingringsmonn
-  nitti_m <- function(yr = 1, dag = lubridate::today(), l = 90) {
-    dag - lubridate::ddays(l) + lubridate::years(yr)
-  }
-  nitti_p <- function(yr = 1, dag = lubridate::today(), l = 90) {
-    dag + lubridate::ddays(l) + lubridate::years(yr)
-  }
-  nitti <- function(yr = 1, dag = lubridate::today(), l = 90) {
-    c(dag - lubridate::ddays(l) + years(yr),
-      dag + lubridate::ddays(l) + lubridate::years(yr))
-  }
+# aarskontrollar
+####################################################
+# +-90                                             #  slingringsmonn
+nitti_m <- function(yr = 1, dag = lubridate::today(), l = 90) {
+  dag - lubridate::ddays(l) + lubridate::years(yr)
+}
+nitti_p <- function(yr = 1, dag = lubridate::today(), l = 90) {
+  dag + lubridate::ddays(l) + lubridate::years(yr)
+}
+nitti <- function(yr = 1, dag = lubridate::today(), l = 90) {
+  c(dag - lubridate::ddays(l) + years(yr),
+    dag + lubridate::ddays(l) + lubridate::years(yr))
+}
 
   dmx <- d_full   # alle operasjoner for kontrollene
 
-  dm <- dmx %>%
-    dplyr::mutate(et_nor_m = nitti_m(dag = Operasjonsdato),
+  dm <- dmx %>%  dplyr::mutate(et_nor_m = nitti_m(dag = Operasjonsdato),
                   et_nor_p = nitti_p(dag = Operasjonsdato),
                   to_nor_m = nitti_m(yr = 2, dag= Operasjonsdato),
                   to_nor_p = nitti_p(yr = 2, dag= Operasjonsdato),
                   pTWL = 100 * (BR_Vekt - `ToAar_Vekt`)/BR_Vekt )
 
-  dn <- dm %>%
-    dplyr::mutate(et_nt= EttAar_Oppfolgingsdato %within% interval( et_nor_m, et_nor_p ),
-                       to_nt= ToAar_Oppfolgingsdato %within% interval( to_nor_m, to_nor_p ),
-                       et_b4= EttAar_Oppfolgingsdato %within% interval( Operasjonsdato+1, et_nor_m-1 ),
-                       et_lt= EttAar_Oppfolgingsdato > et_nor_p,
-                       to_b4= ToAar_Oppfolgingsdato %within% interval( Operasjonsdato+1, to_nor_m-1 ),
-                       to_lt= ToAar_Oppfolgingsdato > to_nor_p)
+dn <- dm %>%
+  dplyr::mutate( et_nt = EttAar_Oppfolgingsdato %within% interval( et_nor_m, et_nor_p ),
+    to_nt = ToAar_Oppfolgingsdato %within% interval( to_nor_m, to_nor_p ),
+    et_b4 = EttAar_Oppfolgingsdato %within%
+      interval( Operasjonsdato+1, et_nor_m-1 ),
+    et_lt = EttAar_Oppfolgingsdato > et_nor_p,
+    to_b4 = ToAar_Oppfolgingsdato %within%
+      interval( Operasjonsdato+1, to_nor_m-1 ),
+    to_lt = ToAar_Oppfolgingsdato > to_nor_p)
+#----------------------------------------------------------------------------80
+dt  <-  dn %>%
+  dplyr::select(
+    c("PasientID", "OperererendeSykehus", "Operasjonsdato", "op_aar",
+      "Operasjonsmetode", "Opmetode_GBP",
+      "et_b4", "et_nt", "et_lt", "to_b4",  "to_nt", "to_lt", "pTWL"))
 
-  dt    <-  dn %>% dplyr::select(  c("PasientID", "OperererendeSykehus", "Operasjonsdato", "op_aar", "Operasjonsmetode", "Opmetode_GBP",
-                                   "et_b4", "et_nt", "et_lt", "to_b4",  "to_nt", "to_lt", "pTWL"))
-
-  ##-----------#------------------------------------------------------------------------------
+##-----------#---------------------------------------------------------------80
 
   # Gjenbrukbar funksjon for å bearbeide Rmd til html
   htmlRenderRmd <- function(srcFile, params = list()) {
@@ -148,137 +164,144 @@ kompl <- function(sh, yr){d_kompl_alv_sjukehus %>% filter(OperererendeSykehus %i
   })
 
 # KI1 - KI6
-#-------------------------------------------------------------------------------------- KI1 figur og tabell
+#---------------------------- KI1 figur og tabell----------------------------80
 
 # lgdgn stats::
   # Viss nokon har *veldig* mange liggedøgn, vert
   # grafen uoversiktleg. Avgrensa derfor talet på
   # liggedøgn me viser grafisk.
-  maksdogn_vis = 14
-  d_prim_6v %<>% dplyr::mutate(liggedogn_lenge = LiggeDogn > maksdogn_vis,
-                               liggedogn_trunk = pmin(LiggeDogn, maksdogn_vis + 1))
-  n_liggedogn_lenge = sum(d_prim_6v$liggedogn_lenge, na.rm=TRUE)
-  liggedogn_maks = max(d_prim_6v$LiggeDogn, na.rm=TRUE)
+maksdogn_vis = 14
+d_prim_6v %<>%
+  dplyr::mutate(
+    liggedogn_lenge = LiggeDogn > maksdogn_vis,
+    liggedogn_trunk = pmin(LiggeDogn, maksdogn_vis + 1))
+n_liggedogn_lenge = sum(d_prim_6v$liggedogn_lenge, na.rm=TRUE)
+liggedogn_maks = max(d_prim_6v$LiggeDogn, na.rm=TRUE)
 
   # Lang figurforklaring viss ikkje pasientane
   # er med i grafen. :)
   # liggedogngraf_forklaring = paste0(
   #   "Talet på postoperative liggedøgn etter primæroperasjonar i~", rapporteringsaar,
   #   ifelse(n_liggedogn_lenge > 0, paste0(", der vi berre viser detaljar for dei med maks ",
-  #                                        maksdogn_vis," postoperative liggedøgn. I tillegg var det ",
-  #                                        n_liggedogn_lenge, " ", ifelse(n_liggedogn_lenge != 1, "pasientar", "pasient"), " med \\emph{meir} enn ",
-  #                                        maksdogn_vis, " postoperative liggedøgn. Pasienten som låg lengst, hadde ",
-  #                                        liggedogn_maks, " postoperative liggedøgn."), "."), " Basert på data frå til saman ",
+  #                     maksdogn_vis," postoperative liggedøgn. I tillegg var det ",
+  #      n_liggedogn_lenge, " ", ifelse(n_liggedogn_lenge != 1, "pasientar", "pasient"), " med \\emph{meir} enn ",
+  #                maksdogn_vis, " postoperative liggedøgn. Pasienten som låg lengst, hadde ",
+  #               liggedogn_maks, " postoperative liggedøgn."), "."), " Basert på data frå til saman ",
   #   num(sum(!is.na(d_prim_6v$LiggeDogn))), " operasjonar ",
   #   "(berre pasientar med seksvekers oppfølging er med).")
+#---------------------------- KI1 figur og tabell----------------------------80
 
   # lgdgn-graf
   # Vis berre dei som låg maks så lenge på grafen
   # Ta med 0 dagar berre om det finst (elles 1)
-  liggedogn_breaks = seq( pmin(1,  min(d_prim_6v$LiggeDogn, na.rm=TRUE)),
-                         maksdogn_vis + 1)
-  liggedogn_tekst = liggedogn_breaks
-  # liggedogn_tekst[length(liggedogn_tekst)] = paste0("\u2265", maksdogn_vis + 1)
+liggedogn_breaks = seq(
+  pmin(1,  min(d_prim_6v$LiggeDogn, na.rm = TRUE)), maksdogn_vis + 1)
+liggedogn_tekst = liggedogn_breaks
+  # liggedogn_tekst[length(liggedogn_tekst)] =
+#                    paste0("\u2265", maksdogn_vis + 1)
 
  ## lggpl <- ggplot2::ggplot(dplyr::filter(d_prim_6v, ! is.na(LiggeDogn)),
  ##           aes(x=liggedogn_trunk, fill=liggedogn_lenge))
   #         geom_bar(stat="count", show.legend = FALSE)
    #  scale_fill_manual(values = c("FALSE"=colPrim[3], "TRUE"=colKontr)) +
-   #  scale_x_continuous(breaks=liggedogn_breaks, labels = liggedogn_tekst, expand=c(0,.6)) +
+   #  scale_x_continuous(breaks=liggedogn_breaks, labels =
+#   liggedogn_tekst, expand=c(0,.6)) +
    #  scale_y_continuous(expand = expand_soyle) +
    #  xlab("Liggedøgn") + ylab("Talet på\npasienter") +
    #  fjern_x
+##-----------#---------------------------------------------------------------80
 
     ## Figur
-  output$PlotKI1 <- renderPlot({
-   soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
+output$PlotKI1 <- renderPlot({
+  soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
   })
   # output$lgdgn <- renderPlot({
   #
   # })
 
-  output$PlotKI2 <- renderPlot({
-    soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
+output$PlotKI2 <- renderPlot({
+  soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
   })
-  output$PlotKI3 <- renderPlot({
-    soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
+output$PlotKI3 <- renderPlot({
+  soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn)
   })
 
   ## Tabell
-  output$TableKI1 <- renderTable({
-   soreg::makeHist(df = d_full, var = input$vrb, bins = input$bn, makeTable = TRUE)
+output$TableKI1 <- renderTable({
+  soreg::makeHist(
+     df = d_full,
+     var = input$vrb,
+     bins = input$bn,
+     makeTable = TRUE)
   })
-  ## Tabell
+## Tabell
 #  output$liggdogn <- renderTable({
 #    DT::dataTableOutput(df = d_full, var = input$vrb, bins = input$bn, makeTable = TRUE)
 #  })
 
- lgg <- reactive({kortligg(input$sh, input$lggar)})
+lgg <- reactive({kortligg(input$sh, input$lggar)})
  output$liggdogn <- DT::renderDataTable({ lgg() })
 
- innl <- reactive({innl30(input$sh, input$lggar)})
+innl <- reactive({innl30(input$sh, input$lggar)})
  output$reinnl <- DT::renderDataTable({ innl() })
 
- kmpl <- reactive({kompl(input$sh, input$lggar)})
+kmpl <- reactive({kompl(input$sh, input$lggar)})
  output$kompl <- DT::renderDataTable({ kmpl() })
 
- ## DT::dataTableOutput(" ligge" )
-#--------------------------------------------------------------------------------------------------------
+## DT::dataTableOutput(" ligge" )
+#----------------------------------------------------------------------------80
   # Samlerapport
   ## vis
-  output$samlerapport <- renderUI({
-    htmlRenderRmd(srcFile = "samlerapport.Rmd",
-                  params = list(var = input$varS, bins = input$binsS))
+output$samlerapport <- renderUI({
+  htmlRenderRmd(srcFile = "samlerapport.Rmd",
+                params = list(var = input$varS, bins = input$binsS))
   })
 
   ## last ned
-  output$downloadSamlerapport <- downloadHandler(
-    filename = function() {
-      "samlerapport.html"
-    },
+output$downloadSamlerapport <- downloadHandler(
+  filename = function() {"samlerapport.html"},
     content = function(file) {
-      srcFile <- normalizePath(system.file("samlerapport.Rmd",
+    srcFile <- normalizePath(system.file("samlerapport.Rmd",
                                            package = "soreg"))
-      tmpFile <- "tmpSamlerapport.Rmd"
+    tmpFile <- "tmpSamlerapport.Rmd"
       owd <- setwd(tempdir())
       on.exit(setwd(owd))
       file.copy(srcFile, tmpFile, overwrite = TRUE)
       out <- rmarkdown::render(tmpFile,
-                               output_format =  rmarkdown::html_document(),
+                               output_format = rmarkdown::html_document(),
                                params = list(var = input$varS,
                                              bins = input$binsS),
                                output_dir = tempdir())
       file.rename(out, file)
     }
   )
-
-
+#----------------------------------------------------------------------------80
   # Abonnement
   ## rekative verdier for å holde rede på endringer som skjer mens
   ## applikasjonen kjører
-  rv <- reactiveValues(
-    subscriptionTab = rapbase::makeUserSubscriptionTab(session))
+rv <- reactiveValues(
+  subscriptionTab = rapbase::makeUserSubscriptionTab(session))
 
   ## lag tabell over gjeldende status for abonnement
-  output$activeSubscriptions <- DT::renderDataTable(
-    rv$subscriptionTab, server = FALSE, escape = FALSE, selection = "none" ,
-    options = list(dom = "tp" , ordning = FALSE), rownames = FALSE
-  )
+output$activeSubscriptions <- DT::renderDataTable(
+  rv$subscriptionTab, server = FALSE, escape = FALSE, selection = "none" ,
+  options = list(dom = "tp" , ordning = FALSE), rownames = FALSE )
 
   ## lag side som viser status for abonnement, også når det ikke finnes noen
-  output$subscriptionContent <- renderUI({
-    userFullName <- rapbase::getUserFullName(session)
-    userEmail <- rapbase::getUserEmail(session)
+output$subscriptionContent <- renderUI({
+  userFullName <- rapbase::getUserFullName(session)
+  userEmail <- rapbase::getUserEmail(session)
     if (length(rv$subscriptionTab) == 0) {
       p(paste("Ingen aktive abonnement for", userFullName))
     } else {
       tagList(
-        p(paste0("Aktive abonnement som sendes per epost til ", userFullName,
+        p(paste0("Aktive abonnement som sendes per epost til ",
+                 userFullName,
                  "(",userEmail, "):")),
         DT::dataTableOutput("activeSubscriptions")
       )
     }
-  })
+})
 
   ## nye abonnement
   observeEvent (input$subscribe, {
@@ -313,7 +336,7 @@ kompl <- function(sh, yr){d_kompl_alv_sjukehus %>% filter(OperererendeSykehus %i
                               interval = interval, intervalName = intervalName)
     rv$subscriptionTab <- rapbase::makeUserSubscriptionTab(session)
   })
-
+#----------------------------------------------------------------------------80
   ## slett eksisterende abonnement
   observeEvent(input$del_button, {
     selectedRepId <- strsplit(input$del_button, "_")[[1]][2]
@@ -339,6 +362,4 @@ kompl <- function(sh, yr){d_kompl_alv_sjukehus %>% filter(OperererendeSykehus %i
   output$meta_data <- renderUI({
     DT::dataTableOutput("meta_table")
   })
-
-
 }
--- inst/shinyApps/soreg/ui.R
+++ inst/shinyApps/soreg/ui.R
@@ -13,53 +13,64 @@ ui <- tagList(
                 regTitle),
     windowTitle = regTitle,
     theme = "rap/bootstrap.css",
-#-----------------------------------------------------------------------------------
-	tabPanel("Start",
-      mainPanel(width = 12,
-        htmlOutput("veiledning", inline = TRUE),
-        shinyalert::useShinyalert(),
-        rapbase::appNavbarUserWidget(user = uiOutput("appUserName"),
-                            organization = uiOutput("appOrgName"),
-                            addUserInfo = TRUE),
+#----------------------------------------------------------------------------80
+tabPanel("Start",
+  mainPanel(width = 12,
+    htmlOutput("veiledning", inline = TRUE),
+      shinyalert::useShinyalert(),
+      rapbase::appNavbarUserWidget(
+        user = uiOutput("appUserName"),
+        organization = uiOutput("appOrgName"),
+        addUserInfo = TRUE
+        ),
         tags$head(tags$link(rel="shortcut icon", href="rap/favicon.ico"))
       )
-    ),
+        ),
 #------------------------------------------------------ KI1 - KI6
-    tabPanel("KI1: Liggedøgn",
+tabPanel("KI1: Liggedøgn",
 	    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-      sidebarPanel(width=3,
-                   pickerInput(
-                     inputId = "sh",
-                     label = "velg sjukehus",
-                     choices =  c("Helse Bergen","Helse Stavanger",
-                                  "Testsjukhus Norge"),   # unique(d_full$OperererendeSykehus),  #
-                     selected = "Testsjukhus Norge",
-                     multiple = TRUE,
-                     options = pickerOptions(
-                       actionsBox = TRUE,
-                       title = "Please select a hospital",
-                       header = "This is a list of hospitals"
-                     )),
-                   checkboxGroupInput(inputId = "lggar",
-                                      label ="år",
-                                      choices = 2014:2020,
-                                      selected = 2016:2018),
-                   dateRangeInput("dato_iv",
-                                  "Operasjonsinterval?",
-                                  start = min_dato,
-                                  end = max_dato),
-                   checkboxGroupInput("op_tech",
-                                      "Operasjonsteknikk",
-                                      choices = c("GS", "GBP", "OA"),
-                                      selected = "GS"),
-                   selectInput(inputId= "vrb",
-                               label = "Variabel:",
-                               c("BR_BMI", "PasientAlder")),
-                   sliderInput("bn",  label = "Antall grupper:",
-                               min = 1, max = 10,
-                               value = 5)
+  sidebarLayout(
+    sidebarPanel(width=3,
+      pickerInput(
+        inputId = "sh",
+        label = "velg sjukehus",
+        choices =  c("Helse Bergen","Helse Stavanger", "Testsjukhus Norge"),
+        # unique(d_full$OperererendeSykehus),  #
+      selected = "Testsjukhus Norge",
+      multiple = TRUE,
+      options = pickerOptions
+      (
+       actionsBox = TRUE,
+       title = "Please select a hospital",
+       header = "This is a list of hospitals"
+      )
       ),
+  checkboxGroupInput(
+    inputId = "lggar",
+    label ="år",
+    choices = 2014:2020,
+    selected = 2016:2018),
+  dateRangeInput(
+    "dato_iv",
+    "Operasjonsinterval?",
+    start = min_dato,
+    end = max_dato),
+  checkboxGroupInput(
+    "op_tech",
+    "Operasjonsteknikk",
+    choices = c("GS", "GBP", "OA"),
+    selected = "GS"),
+  selectInput(
+    inputId = "vrb",
+    label = "Variabel:",
+    c("BR_BMI", "PasientAlder")),
+  sliderInput
+  (
+    inputId = "bn",
+    label = "Antall grupper:",
+    min = 1, max = 10, value = 5
+  )
+    ),
         # sidebarPanel(
         #     sliderInput("bins",
         #                 "Number of bins:",
@@ -79,74 +90,65 @@ ui <- tagList(
         #                        choices = 2014:2020)
 
         # MailPanel
-        mainPanel(
-            navbarPage("Rapporteket",
-          navbarMenu("KI1: Liggedøgn",
-           tabPanel("graf", plotOutput("PlotKI1")),
-           tabPanel("tabell", DT::dataTableOutput("liggdogn"))),
-          navbarMenu("KI2: Reinnleggelse",
-           tabPanel("graf", plotOutput("PlotKI2")),
-           tabPanel("tabell", DT::dataTableOutput("reinnl"))),
-          navbarMenu("KI3: Komplikasjonar",
-           tabPanel("graf", plotOutput("PlotKI3")),
-           tabPanel("tabell", DT::dataTableOutput("kompl"))),
-           tabPanel("KI4: 1-årskrl. nt.", plotOutput("dist4")),
-           tabPanel("KI5: 2-årskrl. nt.", plotOutput("dist5")),
-           tabPanel("KI6: Vekttap >= 20%", plotOutput("dist6"))
-            ),
-           navbarPage("soreg-stuff here",
-                      tabPanel("Samlerapport"
-                               ,
-                               tabPanel("Fordeling av mpg",
-                                        sidebarLayout(
-                                            sidebarPanel(width = 3,
-                                                         selectInput(inputId = "varS",
-                                                                     label = "Variabel:",
-                                                                     c("mpg", "disp", "hp", "drat", "wt", "qsec")),
-                                                         sliderInput(inputId = "binsS",
-                                                                     label = "Antall grupper:",
-                                                                     min = 1,
-                                                                     max = 10,
-                                                                     value = 5),
-                                                         downloadButton("downloadSamlerapport", "Last ned!")
-                                            ),
-                                            mainPanel(
-                                                uiOutput("samlerapport")
-                                            )
-                                        )
-                               )
+    mainPanel(
+      navbarPage("Rapporteket",
+    navbarMenu("KI1: Liggedøgn",
+      tabPanel("graf", plotOutput("PlotKI1")),
+      tabPanel("tabell", DT::dataTableOutput("liggdogn")) ),
+    navbarMenu("KI2: Reinnleggelse",
+      tabPanel("graf", plotOutput("PlotKI2")),
+      tabPanel("tabell", DT::dataTableOutput("reinnl")) ),
+    navbarMenu("KI3: Komplikasjonar",
+      tabPanel("graf", plotOutput("PlotKI3")),
+      tabPanel("tabell", DT::dataTableOutput("kompl")) ),
+      tabPanel("KI4: 1-årskrl. nt.", plotOutput("dist4")),
+      tabPanel("KI5: 2-årskrl. nt.", plotOutput("dist5")),
+      tabPanel("KI6: Vekttap >= 20%", plotOutput("dist6")) ),
+
+#----------------------------------------------------------------------------80
+navbarPage("soreg-stuff here",
+  tabPanel("Samlerapport",
+  tabPanel("Fordeling av mpg",
+    sidebarLayout(
+      sidebarPanel(width = 3,
+      selectInput(
+        inputId = "varS",
+        label = "Variabel:",
+        c("mpg", "disp", "hp", "drat", "wt", "qsec")),
+        sliderInput(
+          inputId = "binsS",
+          label = "Antall grupper:",
+          min = 1, max = 10, value = 5),
+  downloadButton("downloadSamlerapport", "Last ned!") ),
+      mainPanel(uiOutput("samlerapport"))))
                       ),
-                      tabPanel("Abonnement",
-                               sidebarLayout(
-                          sidebarPanel(width = 3,
-                                       selectInput("subscriptionRep",
-                                                   "Rapport:",
-                                                   c("Samlerapport1", "Samlerapport2")),
-                                       selectInput("subscriptionFreq", "Frekvens:",
-                                                   list("\u00c5rlig"="årlig-year",
-                                                        Kvartalsvis="kvartalsvis-quarter",
-                                                        "M\u00e5nedlig"="månedlig-month",
-                                                        Ukentlig="ukentlig-week",
-                                                        Daglig="daglig-DSTday"),
-                                                   selected = "månedlig-month"),
-                                       actionButton("subscribe", "Bestill!")
+    tabPanel("Abonnement",
+  sidebarLayout(
+    sidebarPanel(width = 3,
+      selectInput(
+        inputId = "subscriptionRep",
+        label = "Rapport:",
+        c("Samlerapport1", "Samlerapport2")),
+      selectInput(
+        inputId = "subscriptionFreq",
+        label = "Frekvens:",
+        list("\u00c5rlig" = "årlig-year",
+              Kvartalsvis = "kvartalsvis-quarter",
+          "M\u00e5nedlig" = "månedlig-month",
+                 Ukentlig = "ukentlig-week",
+                   Daglig = "daglig-DSTday"),
+                selected  = "månedlig-month"),
+      actionButton("subscribe", "Bestill!")
                           ),
-                          mainPanel(
-                              uiOutput("subscriptionContent")
-                          )
-                      )),
-                      tabPanel("Metadata",
-                               sidebarLayout(
-                                sidebarPanel(uiOutput("meta_control")),
-                                mainPanel(htmlOutput("meta_data"))
-                               )
-                      )
-                      ) #navbarPage
-        ) #mainPanel
-    ),  #sidebarLayout
-
-
- #-------------------------------------------------------- KI og tabell kan fjernes?
- )
+    mainPanel(uiOutput("subscriptionContent")) )),
+    tabPanel("Metadata",
+  sidebarLayout(
+    sidebarPanel(uiOutput("meta_control")),
+    mainPanel(htmlOutput("meta_data")) ) )
+    ) #navbarPage
+  ) #mainPanel
+),  #sidebarLayout
+#----------------------------------------------------------------------------80
+    )
   ) # navbarPage
 ) # tagList
