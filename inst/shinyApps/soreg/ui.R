45c47475962a7f3cde18aab2dd60e491b030c6c6
Are Edvardsen <biorakel@gmail.com>
2020-11-20 11:15:27 +0100

    more indents

--- inst/shinyApps/soreg/ui.R
+++ inst/shinyApps/soreg/ui.R
@@ -15,72 +15,72 @@ ui <- tagList(
     theme = "rap/bootstrap.css",
     #----------------------------------------------------------------------------80
     tabPanel("Start",
-             mainPanel(width = 12,
-                       shinyalert::useShinyalert(),
-                       rapbase::appNavbarUserWidget(
-                         user = uiOutput("appUserName"),
-                         organization = uiOutput("appOrgName"),
-                         addUserInfo = TRUE
-                       ),
-                       tags$head(tags$link(rel="shortcut icon", href="rap/favicon.ico")),
-                       "Noe om SoReg (foreløpig feil i rendring av Rmd...)"
-                       #htmlOutput("veiledning", inline = TRUE)
-             )
+      mainPanel(width = 12,
+                shinyalert::useShinyalert(),
+                rapbase::appNavbarUserWidget(
+                  user = uiOutput("appUserName"),
+                  organization = uiOutput("appOrgName"),
+                  addUserInfo = TRUE
+                ),
+                tags$head(tags$link(rel="shortcut icon", href="rap/favicon.ico")),
+                "Noe om SoReg (foreløpig feil i rendring av Rmd...)"
+                #htmlOutput("veiledning", inline = TRUE)
+      )
     ),
     #------------------------------------------------------ KI1 - KI6
     tabPanel("KI",
-             sidebarLayout(
-               sidebarPanel( width=3,
-                             pickerInput(
-                               inputId = "sh",
-                               label = "velg sjukehus",
-                               choices =  c("Helse Bergen","Helse Stavanger", "Testsjukhus Norge"),
-                               # unique(d_full$OperererendeSykehus),  #
-                               selected = "Testsjukhus Norge",
-                               multiple = TRUE,
-                               options = pickerOptions
-                               (actionsBox = TRUE,
-                                 title = "Please select a hospital",
-                                 header = "This is a list of hospitals")),
-                             checkboxGroupInput(
-                               inputId = "lggar",
-                               label ="år",
-                               choices = 2014:2020,
-                               selected = 2016:2018),
-                             dateRangeInput(
-                               inputId = "dato_iv",
-                               label = "Operasjonsinterval?",
-                               start = "2018-01-01",
-                               end = "2020-12-31"),
-                             checkboxGroupInput(
-                               inputId = "op_tech",
-                               label = "Operasjonsteknikk",
-                               choices = 1:15,
-                               selected = 6),
-                             selectInput(
-                               inputId = "vrb",
-                               label = "Variabel:",
-                               c("BR_BMI", "PasientAlder")),
-                             sliderInput(
-                               inputId = "bn",
-                               label = "Antall grupper:",
-                               min = 1, max = 10, value = 5
-                             )
-               ),
-               mainPanel(
-                 navbarPage("KI1: Liggedøgn",
-                            tabPanel("graf", plotOutput("lggpl")),
-                            tabPanel("tabell", DT::dataTableOutput("liggdogn")) ),
-                 navbarPage("KI2: Reinnleggelse",
-                            tabPanel("graf", plotOutput("PlotKI2")),
-                            tabPanel("tabell", DT::dataTableOutput("reinnl")) ),
-                 navbarPage("KI3: Komplikasjonar",
-                            tabPanel("graf", plotOutput("PlotKI3")),
-                            tabPanel("tabell", DT::dataTableOutput("kompl")) ),
-                 tabPanel("KI4: 1-årskrl. nt.", plotOutput("dist4")),
-                 tabPanel("KI5: 2-årskrl. nt.", plotOutput("dist5")),
-                 tabPanel("KI6: Vekttap >= 20%", plotOutput("dist6")) ),
-             ), #sidebarlayout
+      sidebarLayout(
+        sidebarPanel(width=3,
+                     pickerInput(
+                       inputId = "sh",
+                       label = "velg sjukehus",
+                       choices =  c("Helse Bergen","Helse Stavanger", "Testsjukhus Norge"),
+                       # unique(d_full$OperererendeSykehus),  #
+                       selected = "Testsjukhus Norge",
+                       multiple = TRUE,
+                       options = pickerOptions
+                       (actionsBox = TRUE,
+                         title = "Please select a hospital",
+                         header = "This is a list of hospitals")),
+                     checkboxGroupInput(
+                       inputId = "lggar",
+                       label ="år",
+                       choices = 2014:2020,
+                       selected = 2016:2018),
+                     dateRangeInput(
+                       inputId = "dato_iv",
+                       label = "Operasjonsinterval?",
+                       start = "2018-01-01",
+                       end = "2020-12-31"),
+                     checkboxGroupInput(
+                       inputId = "op_tech",
+                       label = "Operasjonsteknikk",
+                       choices = 1:15,
+                       selected = 6),
+                     selectInput(
+                       inputId = "vrb",
+                       label = "Variabel:",
+                       c("BR_BMI", "PasientAlder")),
+                     sliderInput(
+                       inputId = "bn",
+                       label = "Antall grupper:",
+                       min = 1, max = 10, value = 5
+                     )
+        ),
+        mainPanel(
+          navbarPage("KI1: Liggedøgn",
+                     tabPanel("graf", plotOutput("lggpl")),
+                     tabPanel("tabell", DT::dataTableOutput("liggdogn")) ),
+          navbarPage("KI2: Reinnleggelse",
+                     tabPanel("graf", plotOutput("PlotKI2")),
+                     tabPanel("tabell", DT::dataTableOutput("reinnl")) ),
+          navbarPage("KI3: Komplikasjonar",
+                     tabPanel("graf", plotOutput("PlotKI3")),
+                     tabPanel("tabell", DT::dataTableOutput("kompl")) ),
+          tabPanel("KI4: 1-årskrl. nt.", plotOutput("dist4")),
+          tabPanel("KI5: 2-årskrl. nt.", plotOutput("dist5")),
+          tabPanel("KI6: Vekttap >= 20%", plotOutput("dist6")) ),
+      ), #sidebarlayout
     ),
     #------------------------------------------------------ KI1 - KI6
     tabPanel("Samlerapport",
