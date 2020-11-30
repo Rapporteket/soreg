52733f29dddee5fcad7b6fe2078637bd615002f4
Hannu Lyyjynen <hapu66@gmail.com>
2020-11-30 12:24:46 +0100

    chp 6.5 declare global variables

--- R/snitt.R
+++ R/snitt.R
@@ -1,8 +1,6 @@
 #' Pick particular hospitals and years from a data frame
 #'
 #' @param df Data frame holding SoReg data
-#' @param OpererendeSykehus operating hospital
-#' @param op_aar operasjonsår
 #' @param sh hospital(s)
 #' @param yr year(s)
 #'
@@ -12,3 +10,7 @@
 
 snitt <- function(df, sh, yr) {df %>%
   dplyr::filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+
+OperererendeSykehus <- op_aar <- NULL
+
+
