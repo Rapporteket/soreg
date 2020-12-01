93ca25bf66785c91da64ae4f027718a2a17c53a7
hapu66 <hapu66@gmail.com>
2020-12-01 13:29:05 +0100

    slingringsmonn

--- R/snitt.R
+++ R/snitt.R
@@ -7,9 +7,46 @@
 #' @return A data frame for choice of hospitals and years
 #'
 #' @export
-
 snitt <- function(df, sh, yr) {df %>%
   dplyr::filter(.data$OperererendeSykehus %in% .env$sh, .data$op_aar %in% .env$yr)}
+  
+#' slingringsmonn for aarskontrollar
+#' ###################################################
+#'
+#' +-90
+#' @param yr years
+#' @param dag operationday
+#' @param l number of deviating controldays
+#' @return beginning day of 'normtid'
+#' @export
+                                             #  slingringsmonn
+  nitti_m <- function(yr = 1, dag = lubridate::today(), l = 90) {
+    dag - lubridate::ddays(l) + lubridate::years(yr)
+  }
+  
+#' @param yr years
+#' @param dag operationday
+#' @param l number of deviating controldays
+#' @return end day of 'normtid'
+#' @export
+  
+  nitti_p <- function(yr = 1, dag = lubridate::today(), l = 90) {
+    dag + lubridate::ddays(l) + lubridate::years(yr)
+  }
+
+#' @param yr years
+#' @param dag operationday
+#' @param l number of deviating controldays
+#' @return interval of 'normtid'
+#' @export
+
+  nitti <- function(yr = 1, dag = lubridate::today(), l = 90) {
+    c(dag - lubridate::ddays(l) + years(yr),
+      dag + lubridate::ddays(l) + lubridate::years(yr))
+  }
+  
+ 
+
 
 
 
