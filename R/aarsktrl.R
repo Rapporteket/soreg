06f9d3650edd2e61443c899f4d498f11c5b3807f
Hannu Lyyjynen <hapu66@gmail.com>
2020-12-02 13:31:20 +0100

    aarkt

--- R/aarsktrl.R
+++ R/aarsktrl.R
@@ -12,36 +12,38 @@ aars_ktrl <- function(df){
 df <- df %>%  dplyr::mutate(et_nor_m = nitti_m(dag = Operasjonsdato),
                            et_nor_p = nitti_p(dag = Operasjonsdato),
                            to_nor_m = nitti_m(yr = 2, dag = Operasjonsdato),
-						   to_nor_p = nitti_p(yr = 2, dag = Operasjonsdato),
+			   to_nor_p = nitti_p(yr = 2, dag = Operasjonsdato),
                            pTWL = 100*(BR_Vekt - `ToAar_Vekt`)/BR_Vekt )
 df <- df %>% dplyr::mutate(
- et_nt = EttAar_Oppfolgingsdato %within% interval( et_nor_m, et_nor_p ),
- to_nt = ToAar_Oppfolgingsdato %within% interval( to_nor_m, to_nor_p ),
+ et_nt = EttAar_Oppfolgingsdato %within%
+         lubridate::interval( et_nor_m, et_nor_p ),
+ to_nt = ToAar_Oppfolgingsdato %within%
+         lubridate::interval( to_nor_m, to_nor_p ),
  et_b4 = EttAar_Oppfolgingsdato %within%
-         interval( Operasjonsdato+1, et_nor_m-1 ),
+         lubridate::interval( Operasjonsdato+1, et_nor_m-1 ),
  et_lt = EttAar_Oppfolgingsdato > et_nor_p,
  to_b4 = ToAar_Oppfolgingsdato %within%
-         interval( Operasjonsdato+1, to_nor_m-1 ),
+         lubridate::interval( Operasjonsdato+1, to_nor_m-1 ),
  to_lt = ToAar_Oppfolgingsdato > to_nor_p) %>% dplyr::select(
  c("PasientID", "OperererendeSykehus", "Operasjonsdato", "op_aar",
  "Operasjonsmetode", "Opmetode_GBP", "et_b4", "et_nt", "et_lt",
  "to_b4",  "to_nt", "to_lt", "pTWL"))
 
 last_opday <- max(df$Operasjonsdato)
-k1_last <- last_opday - lubridate::months(15) # slingringsmonn
-k2_last <- last_opday - lubridate::months(27)
+k1_last <- last_opday -  months(15) # slingringsmonn
+k2_last <- last_opday -  months(27) #
 
 k1nt  <-  df %>%
  dplyr::filter(Operasjonsdato < k1_last) %>%
- dplyr::group_by(OperererendeSykehus,op_aar) %>% dplyr::mutate(ops = n()) %>%
+ dplyr::group_by(OperererendeSykehus, op_aar) %>% dplyr::mutate(ops = dplyr::n()) %>%
  dplyr::summarise(ktr1 = sum(et_nt, na.rm = T), oprs = ops[1],
            kt1 = sum(et_nt, na.rm = T)/ ops[1])
 
 k2nt  <-  df %>%
  dplyr::filter(Operasjonsdato < k2_last) %>%
- dplyr::group_by(OperererendeSykehus,op_aar) %>% dplyr::mutate(ops = n()) %>%
+ dplyr::group_by(OperererendeSykehus, op_aar) %>% dplyr::mutate(ops = dplyr::n()) %>%
  dplyr::summarise(ktr2 = sum(to_nt, na.rm = T),oprs = ops[1],
-		    kt2 = sum(to_nt, na.rm = T)/ ops[1])
+           kt2 = sum(to_nt, na.rm = T)/ ops[1])
 }
 #--------------------------------------------------------------------------- 80
 #' Pick particular hospitals and years from a data frame
@@ -55,7 +57,7 @@ k2nt  <-  df %>%
 #' @export
 
 k1 <- function(sh, yr) {k1nt %>%
-        filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+dplyr::filter(.data$OperererendeSykehus %in% .env$sh, .data$op_aar %in% .env$yr)}
 
 #' Pick particular hospitals and years from a data frame
 #'
@@ -68,7 +70,7 @@ k1 <- function(sh, yr) {k1nt %>%
 #' @export
 
 k2 <- function(sh, yr) {k2nt %>%
-        filter(OperererendeSykehus %in% sh, op_aar %in% yr)}
+dplyr::filter(.data$OperererendeSykehus %in% .env$sh, .data$op_aar %in% .env$yr)}
 
 #--------------------------------------------------------------------------- 80
 
