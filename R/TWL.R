826c004e6b25e1b14f9ca04bbff0c6a94cacbe21
Hannu Lyyjynen <hapu66@gmail.com>
2020-12-04 12:49:08 +0100

    .data vs df

--- R/TWL.R
+++ R/TWL.R
@@ -10,18 +10,18 @@
 TWL <- function(dt){
 d_GS    <- dt %>% dplyr::filter(.data$Operasjonsmetode == 6)
 d_RYGBP <- dt %>% dplyr::filter(.data$Operasjonsmetode == 1,
-                                .data$Opmetode_GBP == 1)   # mini = .data$Opmetode_GBP
+.data$Opmetode_GBP == 1)   # mini = .data$Opmetode_GBP
 d_OAGB  <- dt %>% dplyr::filter(.data$Operasjonsmetode == 1,
-                                .data$Opmetode_GBP == 2)
+.data$Opmetode_GBP == 2)
 
 d_TWL  <- dt %>% dplyr::filter(.data$Operasjonsmetode %in% c(1, 6)) %>%
-                 dplyr::filter(!is.na(.data$TWL)) %>% dplyr::mutate(del20 = .data$TWL >= 20.0)
+dplyr::filter(!is.na(.data$TWL)) %>% dplyr::mutate(del20 = .data$TWL >= 20.0)
 
 d_slv  <- d_TWL %>% dplyr::filter(.data$Operasjonsmetode == 6)
 d_gbp  <- d_TWL %>% dplyr::filter(.data$Operasjonsmetode == 1,
-                                  .data$Opmetode_GBP == 1)
+.data$Opmetode_GBP == 1)
 d_oa   <- d_TWL %>% dplyr::filter(.data$Operasjonsmetode == 1,
-                                  .data$Opmetode_GBP == 2)
+.data$Opmetode_GBP == 2)
 
 detail    <-  function(dmx) {dmx %>%
  dplyr::group_by(.data$OperererendeSykehus, .data$op_aar)}
@@ -38,16 +38,16 @@ gbp20   <-    detail(d_gbp) %>%
 
 slvdel20 <- function(sh, yr) {slv20 %>%
 dplyr::filter(.data$OperererendeSykehus %in% .data$sh,
-              .data$op_aar %in% .data$yr)}
+ .data$op_aar %in% .data$yr)}
 gbpdel20 <- function(sh, yr) {gbp20 %>%
 dplyr::filter(.data$OperererendeSykehus %in% .data$sh,
-              .data$op_aar %in% .data$yr)}
+ .data$op_aar %in% .data$yr)}
  oadel20 <- function(sh, yr) { oa20 %>%
  dplyr::filter(.data$OperererendeSykehus %in% .data$sh,
-               .data$op_aar %in% .data$yr)}
+ .data$op_aar %in% .data$yr)}
 
 tb <-     dt %>% dplyr::group_by(.data$OperererendeSykehus) %>%
-                 dplyr::summarise( "to år %TWL"= mean(.data$pTWL, na.rm = TRUE))
+ dplyr::summarise( "to år %TWL"= mean(.data$pTWL, na.rm = TRUE))
 
 tb20 <-   dt %>% dplyr::mutate(del20 = .data$pTWL>=20.0 )  %>%
  dplyr::group_by(.data$OperererendeSykehus) %>%
@@ -92,8 +92,7 @@ GS20 <- function(sh, yr) {.data$d_GS %>%  dplyr::mutate(del20 = .data$pTWL >= 20
 dplyr::filter(.data$OperererendeSykehus %in% .data$sh, .data$op_aar %in% .data$yr) %>%
 dplyr::group_by(.data$OperererendeSykehus) %>%
 dplyr::filter(.data$to_nt) %>%
-dplyr::summarise("ops"= dplyr::n(),
-                  "del %TWL>=20.0" = mean(.data$del20, na.rm = TRUE))}
+dplyr::summarise("ops"= dplyr::n(),"del %TWL>=20.0" = mean(.data$del20, na.rm = TRUE))}
 
 #--------------------------------------------------------------------------- 80
 #' Pick particular hospitals and years from a data frame
@@ -106,9 +105,7 @@ dplyr::summarise("ops"= dplyr::n(),
 #' @export
 
 GB20 <- function(sh, yr)
-        {.data$d_RYGBP %>% dplyr::mutate(del20 = .data$pTWL >= 20.0 ) %>%
- dplyr::filter(.data$OperererendeSykehus %in% .data$sh,
-               .data$op_aar %in% .data$yr) %>%
+{.data$d_RYGBP %>% dplyr::mutate(del20 = .data$pTWL >= 20.0 ) %>%
+ dplyr::filter(.data$OperererendeSykehus %in% .data$sh, .data$op_aar %in% .data$yr) %>%
  dplyr::group_by(.data$OperererendeSykehus) %>% dplyr::filter(.data$to_nt) %>%
- dplyr::summarise("ops" = dplyr::n(),
-                  "del %TWL>=20.0" = mean(.data$del20, na.rm = TRUE))}
+ dplyr::summarise("ops" = dplyr::n(), "del %TWL>=20.0" = mean(.data$del20, na.rm = TRUE))}
