9e7151698e117b3dbbf9dc2e534217426762696d
Are Edvardsen <biorakel@gmail.com>
2021-08-04 10:48:25 +0200

    clean-up

--- inst/shinyApps/soreg/ui.R
+++ inst/shinyApps/soreg/ui.R
@@ -2,14 +2,15 @@ addResourcePath("rap", system.file("www", package = "rapbase"))
 regTitle <- "SoReg"
 
 ui <- shiny::tagList(
-shiny::navbarPage(
-title = shiny::div(a(includeHTML(system.file("www/logo.svg",
-                                             package = "rapbase"))),
-                regTitle),
+  shiny::navbarPage(
+    title = shiny::div(a(includeHTML(system.file("www/logo.svg",
+                                                 package = "rapbase"))),
+                       regTitle),
     windowTitle = regTitle,
     theme = "rap/bootstrap.css",
     id = "tabs",
-    shiny::tabPanel("Start",
+    shiny::tabPanel(
+      "Start",
       mainPanel(
         width = 12,
         shiny::htmlOutput("veiledning", inline = TRUE),
@@ -21,10 +22,12 @@ title = shiny::div(a(includeHTML(system.file("www/logo.svg",
         )
       )
     ),
-    shiny::tabPanel("Kvalitetsindikatorer",
+    shiny::tabPanel(
+      "Kvalitetsindikatorer",
       shiny::sidebarLayout(
-        shiny::sidebarPanel(width = 3,
-           shiny::uiOutput("kIix"),
+        shiny::sidebarPanel(
+          width = 3,
+          shiny::uiOutput("kIix"),
           shiny::uiOutput("uc_sh"),
           shiny::uiOutput("uc_years"),
           shiny::uiOutput("uc_prim"),
@@ -32,25 +35,32 @@ title = shiny::div(a(includeHTML(system.file("www/logo.svg",
           shiny::uiOutput("uc_oagb"), #
           shiny::uiOutput("uc_dates")
         ),
-        shiny::mainPanel(width = 9,
-        shiny::tabsetPanel(
-          shiny::tabPanel("Tabell", shiny::uiOutput("dT")),
-          shiny::tabPanel("Figur", shiny::plotOutput("graf"))
-        )))),
-shiny::tabPanel("Datadump",
-    shiny::sidebarLayout(
-    shiny::sidebarPanel(
-        width = 4,
-        uiOutput("dumpTabControl"),
-        dateRangeInput("dumpDateRange", "Velg periode:",
-                       start = lubridate::ymd(Sys.Date()) - lubridate::years(1),
-                       end = Sys.Date(), separator = "-",
-                       weekstart = 1),
-        radioButtons("dumpFormat", "Velg filformat:",
-                      choices = list(csv = "csv",
-                                  `csv2 (nordisk format)` = "csv2",
-                                  `xlsx-csv` = "xlsx-csv",
-                                  `xlsx-csv2 (nordisk format)` = "xlsx-csv2")),
+        shiny::mainPanel(
+          width = 9,
+          shiny::tabsetPanel(
+            shiny::tabPanel("Tabell", shiny::uiOutput("dT")),
+            shiny::tabPanel("Figur", shiny::plotOutput("graf"))
+          )
+        )
+      )
+    ),
+    shiny::tabPanel(
+      "Datadump",
+      shiny::sidebarLayout(
+        shiny::sidebarPanel(
+          width = 4,
+          uiOutput("dumpTabControl"),
+          dateRangeInput(
+            "dumpDateRange", "Velg periode:",
+            start = lubridate::ymd(Sys.Date()) - lubridate::years(1),
+            end = Sys.Date(), separator = "-",
+            weekstart = 1),
+          radioButtons(
+            "dumpFormat", "Velg filformat:",
+            choices = list(csv = "csv",
+                           `csv2 (nordisk format)` = "csv2",
+                           `xlsx-csv` = "xlsx-csv",
+                           `xlsx-csv2 (nordisk format)` = "xlsx-csv2")),
           downloadButton("dumpDownload", "Hent!")
         ),
         shiny::mainPanel(
