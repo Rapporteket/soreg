6cc59dccae3e70f28fc82f00cde58655c4343e70
Hannu Lyyjynen <hannu.sakari.lyyjynen@helse-bergen.no>
2023-01-30 09:31:27 +0100

    forenkle

--- R/snitt.R
+++ R/snitt.R
@@ -370,20 +370,13 @@ aarKtrl <- function(df, k, agg){
 
 
 aar_ktr_gr <- function(df, k) {
-#  nt <- et_nt <- to_nt <- NULL
-  # last_opday <- max(df$Operasjonsdato)
-  # switch(k,
-  #        "1" = {
-  #          last_op <-  last_opday - months(15)},
-  #        "2" = {
-  #          last_op <-  last_opday - months(27)})
+
   df <- df %>%
     dplyr::mutate(
       et_nor_m = nitti_m(yr = 1, dag = .data$Operasjonsdato, l = 90),
       et_nor_p = nitti_p(yr = 1, dag = .data$Operasjonsdato, l = 90),
       to_nor_m = nitti_m(yr = 2, dag = .data$Operasjonsdato, l = 90),
       to_nor_p = nitti_p(yr = 2, dag = .data$Operasjonsdato, l = 90),
-  #    pTWL = 100 * (.data$BR_Vekt - .data$a2_Vekt) / .data$BR_Vekt,
       et_nt = .data$a1_KontrollDato %within%
         lubridate::interval(.data$et_nor_m, .data$et_nor_p),
       to_nt = .data$a2_KontrollDato %within%
